6075b5af628a4e84c899cecedbeb56886b98acf1
Dmytro Bondar <Dmytro Bondar>
2017-02-08 19:50:04 +0200

    added set of needed pages

--- src/main/resources/static/js/letsgo.js
+++ src/main/resources/static/js/letsgo.js
@@ -1,20 +1,131 @@
 var letsgo = angular.module('letsgo', ['ui.router', 'mgcrea.ngStrap', 'ngResource', 'spring-data-rest']);
 letsgo.config(function ($stateProvider) {
-    var helloState = {
-        name: 'hello',
-        url: '/hello',
-        templateUrl: "hello.html"
-    };
-
     var aboutState = {
         name: 'about',
         url: '/about',
         templateUrl: "about.html"
     };
-
-    $stateProvider.state(helloState);
     $stateProvider.state(aboutState);
+    
+    var addAdminState = {
+        name: 'addAdmin',
+        url: '/addAdmin',
+        templateUrl: "addAdmin.html"
+    };
+    $stateProvider.state(addAdminState);
+    
+    var addEventState = {
+        name: 'addEvent',
+        url: '/addEvent',
+        templateUrl: "addEvent.html"
+    };
+    $stateProvider.state(addEventState);
+
+    var homeState = {
+        name: 'home',
+        url: '/home',
+        templateUrl: "home.html"
+    };
+
+    $stateProvider.state(homeState);
+
+    var logInAdminState = {
+        name: 'logInAdmin',
+        url: '/admin/log',
+        templateUrl: "logInAdmin.html"
+    };
+    
+    $stateProvider.state(logInAdminState);
+    
+    var logInOwnerState = {
+        name: 'logInOwner',
+        url: '/owner/log',
+        templateUrl: "logInOwner.html"
+    };
+
+    $stateProvider.state(logInOwnerState);
+    
+    var logInVisitorState = {
+        name: 'logInVisitor',
+        url: '/visitor/log',
+        templateUrl: "logInVisitor.html"
+    };
+
+    $stateProvider.state(logInVisitorState);
+
+    var manageAdminState = {
+        name: 'manageAdmin',
+        url: '/admin/manage',
+        templateUrl: "manageAdmin.html"
+    };
+
+    $stateProvider.state(manageAdminState);
+    
+    var manageEventsState = {
+        name: 'manageEvents',
+        url: '/event/manage',
+        templateUrl: "manageEvents.html"
+    };
+
+    $stateProvider.state(manageEventsState);
+    
+    var manageOwnersState = {
+        name: 'manageOwners',
+        url: '/owner/manage',
+        templateUrl: "manageOwners.html"
+    };
+
+    $stateProvider.state(manageOwnersState);
+    
+    var manageTicketsState = {
+        name: 'manageTickets',
+        url: '/ticket/manage',
+        templateUrl: "manageTickets.html"
+    };
+
+    $stateProvider.state(manageTicketsState);
+    
+    var registerOwnerState = {
+        name: 'registerOwner',
+        url: '/owner/register',
+        templateUrl: "registerOwner.html"
+    };
+
+    $stateProvider.state(registerOwnerState);
+    
+    var registerVisitorState = {
+        name: 'registerVisitor',
+        url: '/visitor/register',
+        templateUrl: "registerVisitor.html"
+    };
+
+    $stateProvider.state(registerVisitorState);
+    
+    var searchEventState = {
+        name: 'searchEvent',
+        url: '/event/search',
+        templateUrl: "searchEvent.html"
+    };
+
+    $stateProvider.state(searchEventState);
+    
+    var viewEventState = {
+        name: 'viewEvent',
+        url: '/event/view',
+        templateUrl: "viewEvent.html"
+    };
+
+    $stateProvider.state(viewEventState);
+    
+    var viewTicketState = {
+        name: 'viewTicket',
+        url: '/ticket/view',
+        templateUrl: "viewTicket.html"
+    };
+
+    $stateProvider.state(viewTicketState);
 });
+
 letsgo.controller("main", function ($scope) {
     $scope.name = "User Name";
 });
