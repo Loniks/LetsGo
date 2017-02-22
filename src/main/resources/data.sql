7a365ea38833f612024a62d8b4e3147897c1f346
Dmytro Bondar <work.bondar@gmail.com>
2017-02-22 20:22:40 +0200

    updated data.sql for posgresql 9.2 syntax

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,12 @@
-INSERT INTO admins(
+DELETE FROM admins WHERE id=1;
+INSERT  INTO admins(
 	id, email, name, password, role, status)
-	VALUES (1, 'supper@test.com', 'Supper Admin', 'supper', 'ROLE_SUPPER', 'enable') ON CONFLICT DO NOTHING;
+	VALUES (1, 'supper@test.com', 'Supper Admin', 'supper', 'ROLE_SUPPER', 'enable');
+DELETE FROM sponsors WHERE id=1;
 INSERT INTO sponsors(
   id, contact_person, email, name, password, phone, status)
-VALUES (1, 'Steve Jonson', 'bigcompany@company.com', 'Big company','bigPass', '0123456789', 'enable') ON CONFLICT DO NOTHING;
+VALUES (1, 'Steve Jonson', 'bigcompany@company.com', 'Big company','bigPass', '0123456789', 'enable');
+DELETE FROM visitors WHERE id=1;
 INSERT INTO visitors(
   id, email, name, password, phone, status)
-VALUES (1, 'goodVisitor@visitor.com', 'Mike Pitteson', 'goodPass', '03211236547', 'enable') ON CONFLICT DO NOTHING;
+VALUES (1, 'goodVisitor@visitor.com', 'Mike Pitteson', 'goodPass', '03211236547', 'enable');
