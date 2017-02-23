7b8172dd8379d227ca67b70daf15cce8bc04c128
dimangry <8realdima8@rambler.ru>
2017-02-23 11:07:30 +0200

    return hashtag in url  and setting compile sass in css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    sass = require('gulp-sass');
 
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -10,3 +11,16 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
         notify: false // Отключаем уведомления
     });
 });
+
+gulp.task('sass', function() {
+    return gulp.src('src/main/resources/static/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/main/resources/static/css'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('src/main/resources/static/sass/**/*.scss', ['sass']);
+    gulp.watch('src/main/resources/static/**/*.html', browserSync.reload);
+    gulp.watch('src/main/resources/static/js/**/*.js', browserSync.reload);
+})
