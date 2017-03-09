e0d6e06eddf7bb3787312eb22c4037d003a23241
dimangry <8realdima8@rambler.ru>
2017-03-09 15:40:53 +0200

    update view config

---
+++ src/main/resources/static/js/routing.js
@@ -0,0 +1,52 @@
+angular.module('loginApp').config(['$stateProvider', '$urlRouterProvider', 'USER_ROLES',
+    function($stateProvider, $urlRouterProvider, USER_ROLES) {
+
+        // For any unmatched url, redirect to /
+        $urlRouterProvider.otherwise("/");
+
+        // Now set up the states
+        $stateProvider
+            .state('home', {
+                url: "/",
+                templateUrl: "home.html",
+                data: {
+                    authorizedRoles: [USER_ROLES.admin, USER_ROLES.owner]
+                }
+            })
+            .state('state1', {
+                url: "/state1",
+                templateUrl: "state1.html",
+                data: {
+                    authorizedRoles: [USER_ROLES.admin]
+                }
+            })
+            .state('state2', {
+                url: "/state2",
+                templateUrl: "state2.html",
+                data: {
+                    authorizedRoles: [USER_ROLES.admin]
+                }
+            })
+            .state('manageEvents', {
+                url: "/manageEvents",
+                templateUrl: "manageEvents.html",
+                data: {
+                    authorizedRoles: [USER_ROLES.owner]
+                }
+            })
+            .state('manageOwners', {
+                url: "/manageOwners",
+                templateUrl: "manageOwners.html",
+                data: {
+                    authorizedRoles: [USER_ROLES.admin]
+                }
+            })
+            .state('manageTickets', {
+                url: "/manageTickets",
+                templateUrl: "manageTickets.html",
+                data: {
+                    authorizedRoles: [USER_ROLES.admin]
+                }
+            });
+    }
+]);
