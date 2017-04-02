547a1abf7df193123b8ba5cfd4c1bdea50e00610
Dmytro Bondar <Dmytro Bondar>
2017-04-02 17:03:08 +0300

    romoved frontend folder for hotswap

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,23 @@ var gulp = require('gulp'),
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
         server: { // Определяем параметры сервера
-            baseDir: 'src/main/resources/static/' // Директория для сервера - app
+            baseDir: 'src/main/webapp/' // Директория для сервера - app
         },
         notify: false // Отключаем уведомления
     });
 });
 
 gulp.task('sass', function() {
-    return gulp.src('src/main/resources/static/sass/**/*.scss')
+    return gulp.src('src/main/webapp/sass/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('src/main/resources/static/css'))
+        .pipe(gulp.dest('src/main/webapp/css'))
         .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('watch', ['browser-sync', 'sass'], function() {
-    gulp.watch('src/main/resources/static/sass/**/*.scss', ['sass']);
-    gulp.watch('src/main/resources/static/**/*.html', browserSync.reload);
-    gulp.watch('src/main/resources/static/js/**/*.js', browserSync.reload);
+    gulp.watch('src/main/webapp/sass/**/*.scss', ['sass']);
+    gulp.watch('src/main/webapp/**/*.html', browserSync.reload);
+    gulp.watch('src/main/webapp/js/**/*.js', browserSync.reload);
 })
 
 gulp.task('default', ['watch']);
