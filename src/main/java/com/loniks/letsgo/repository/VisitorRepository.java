35168df261ba3bcc6348ebeda2b8229bdda9d3d8
Dmytro Bondar <work.bondar@gmail.com>
2017-04-08 20:09:21 +0300

    added some logic for db auntetification

--- src/main/java/com/loniks/letsgo/repository/VisitorRepository.java
+++ src/main/java/com/loniks/letsgo/repository/VisitorRepository.java
@@ -2,6 +2,7 @@ package com.loniks.letsgo.repository;
 
 import com.loniks.letsgo.domain.VisitorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RepositoryRestResource(path = "visitors")
 public interface VisitorRepository extends JpaRepository<VisitorEntity,Long> {
+    VisitorEntity findOneByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
 }
