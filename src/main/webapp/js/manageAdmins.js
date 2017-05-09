b1fab77d2dba16fe870a49b614fd73e7ae90469e
Dmytro Bondar <work.bondar@gmail.com>
2017-05-09 17:24:46 +0300

    added footer and updated auth logic

--- src/main/webapp/js/manageAdmins.js
+++ src/main/webapp/js/manageAdmins.js
@@ -2,21 +2,20 @@
 
 
 angular.module('letsgo')
-    .controller('manageAdmins', function ($scope, $http, user, $window, Auth) {
+    .controller('manageAdmins', function ($scope, $http) {
         $scope.admins = null;
         findAllAdmins();
 
         $scope.changeStatus = function changeStatus(admin) {
             if (admin.status == "enable") admin.status = "disable";
             else admin.status = "enable";
-            alert(admin.status);
-            $http.post('api/admins', admin).then(function () {
+            $http.post('api/admins', admin).finally(function () {
                 findAllAdmins();
             });
         };
 
         $scope.deleteAdmin = function deleteAdmin(admin) {
-            $http.delete('api/admins', admin).then(function () {
+            $http.delete('api/admins', admin).finally(function () {
                 findAllAdmins();
             });
         };
@@ -25,7 +24,7 @@ angular.module('letsgo')
 
             $http.get('api/admins')
                 .then(function (data) {
-                    $scope.admins = data._embedded.adminEntities;
+                    $scope.admins = data.data._embedded.adminEntities;
                 });
         }
     });
