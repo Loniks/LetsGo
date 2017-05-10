19d8ecd1fc68bba60a1ec85a4def92a754cae6e6
Dmytro Bondar <work.bondar@gmail.com>
2017-05-10 22:13:08 +0300

    added seach event and manage ticket logic

--- src/main/java/com/loniks/letsgo/repository/EventRepository.java
+++ src/main/java/com/loniks/letsgo/repository/EventRepository.java
@@ -3,8 +3,11 @@ package com.loniks.letsgo.repository;
 import com.loniks.letsgo.domain.EventEntity;
 import com.loniks.letsgo.domain.SponsorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Bondar Dmytro
@@ -12,5 +15,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RepositoryRestResource(path = "events")
 public interface EventRepository extends JpaRepository<EventEntity,Long> {
+
+    List<EventEntity> findByNameContaining(@Param("name") String name);
 
 }
