096ef8513f5124111366f03eaecd50d37ddb468e
Dmytro Bondar <dbondar@chegg.com>
2017-12-16 11:47:39 +0200

    added changes in main class

--- src/main/java/com/loniks/letsgo/LetsGoApplication.java
+++ src/main/java/com/loniks/letsgo/LetsGoApplication.java
@@ -1,7 +1,13 @@
 package com.loniks.letsgo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 @SpringBootApplication
 public class LetsGoApplication {
@@ -9,4 +15,22 @@ public class LetsGoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LetsGoApplication.class, args);
 	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter primaryMessageConverter(){
+
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setObjectMapper(getObjectMapper());
+		return converter;
+	}
+
+	@Bean
+	public ObjectMapper getObjectMapper(){
+		ObjectMapper objectMapper = new ObjectMapper();
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		objectMapper.setDateFormat(df);
+		return objectMapper;
+	}
+
+
 }
